d3d09cdce3fbdd6cad73e0a542edeb8bd1bcc986
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-11-03 13:55:50 -0300

    SE QUITO UN

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 
 export const environment = {
-    PORT: process.env.PORT || 5000,
-    SECRET: process.env.SECRET || '123456789',
-    DB: {
-      DB_NAME: process.env.DB_NAME || 'encuesta',
-      DB_HOST: process.env.DB_HOST || 'localhost',
-      DB_DIALECT: process.env.DB_DIALECT || 'postgres',
-      DB_USER: process.env.DB_USER || 'postgres',
-      DB_PASSWORD: process.env.DB_PASSWORD || '24879156',
-      DB_PORT: process.env.DB_PORT || 5432
-    }
-  };
+  PORT: process.env.PORT || 5000,
+  SECRET: process.env.SECRET || '123456789',
+  DB: {
+    DB_NAME: process.env.DB_NAME || 'encuesta',
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_DIALECT: process.env.DB_DIALECT || 'postgres',
+    DB_USER: process.env.DB_USER || 'postgres',
+    DB_PASSWORD: process.env.DB_PASSWORD || '123456',
+    DB_PORT: process.env.DB_PORT || 5432
+  }
+};
